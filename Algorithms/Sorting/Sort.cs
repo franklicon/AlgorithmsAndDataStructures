6c7306f1ff84f7ad22ca66cb67d6f8ffcb5091a4
Francisco Licon <FranciscoArturo.Licon@Honeywell.com>
2024-02-21 11:53:33 -0600

    Add project files.

---
+++ Algorithms/Sorting/Sort.cs
@@ -0,0 +1,33 @@
+﻿namespace Algorithms.Sorting
+{
+    public static class Sort<T> where T : IComparable
+    {
+        /// <summary>
+        /// Sorts an one-dimensional array using bubble sort.
+        /// </summary>
+        /// <param name="array">The one-dimensional array to sort.</param>
+        public static void BubbleSort(T[] array)
+        {
+            var n = array.Length;
+            for (var i = 0; i < n - 1; i++)
+            {
+                for (var j = 0; j < n - i - 1; j++)
+                {
+                    if (array[j + 1].CompareTo(array[j]) < 0)
+                    {
+                        (array[j + 1], array[j]) = (array[j], array[j + 1]);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Sorts an one-dimensional array using selection sort.
+        /// </summary>
+        /// <param name="array">The one-dimensional array to sort.</param>
+        /// <exception cref="NotSupportedException"></exception>
+        public static void SelectionSort(T[] array)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ AlgorithmsTests/SortTests.cs
@@ -0,0 +1,48 @@
+﻿using Algorithms.Sorting;
+using Xunit;
+
+namespace AlgorithmsTests
+{
+    public class SortTests
+    {
+        private void SortTest_CharArray(Action<char[]> sort)
+        {
+            // Arrange
+            var array = new char[] { 'b', 'z', 'c', 't', 'a', 'f', 'x' };
+            var expectedArray = new char[] { 'a', 'b', 'c', 'f', 't', 'x', 'z' };
+
+            // Act
+            sort(array);
+
+            // Assert
+            Assert.True(array.SequenceEqual(expectedArray));
+        }
+
+        private void SortTest_IntArray(int min, int max, Action<int[]> sort)
+        {
+            // Arrange 
+            var random = new Random();
+            var array = Enumerable.Repeat(0, max).Select(i => random.Next(min, max)).ToArray();
+            var expectedArray = new int[array.Length];
+            for (var i = 0; i < expectedArray.Length; i++)
+            { expectedArray[i] = array[i]; }
+
+            // Act
+            sort(array);
+            Array.Sort(expectedArray);
+
+            // Assert
+            Assert.True(array.SequenceEqual(expectedArray));
+        }
+
+        [Fact]
+        public void RunSortTests()
+        {
+            // Bubble Sort
+            SortTest_IntArray(0, 100, Sort<int>.BubbleSort);
+            SortTest_CharArray(Sort<char>.BubbleSort);
+
+            // Selection Sort.
+        }
+    }
+}

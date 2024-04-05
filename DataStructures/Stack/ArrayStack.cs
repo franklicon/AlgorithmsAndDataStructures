745e22a04629419c0a96cbb29aa8f5d7be23b9a5
Francisco Licon <FranciscoArturo.Licon@Honeywell.com>
2024-04-04 22:46:53 -0600

    Add ArrayStack

---
+++ DataStructures/Stack/ArrayStack.cs
@@ -0,0 +1,50 @@
+﻿namespace DataStructures.Stack
+{
+    public class ArrayStack<T>
+    {
+        public int Count { get; private set; }
+        public bool IsEmpty => Count == 0;
+        private T[] stack;
+        private const int defaultCapacity = 10;
+
+        public ArrayStack()
+        {
+            stack = new T[defaultCapacity];
+        }
+
+        public ArrayStack(int capacity)
+        {
+            stack = new T[capacity];
+        }
+
+        public void Push(T item)
+        {
+            if(stack.Length == Count)
+            {
+                Array.Resize(ref stack, Count * 2);
+            }
+
+            stack[Count++] = item;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return stack[--Count];
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+
+            return stack[Count - 1];
+        }
+    }
+}
---
+++ DataStructuresTests/StackTests.cs
@@ -0,0 +1,66 @@
+﻿using DataStructures.LinkedList;
+using DataStructures.Stack;
+using Xunit;
+
+
+namespace DataStructuresTests
+{
+    public class StackTests
+    {
+        [Fact]
+        public void ArrayStack_IsEmpty_Test()
+        {
+            // Arrange
+            var arrayStack = new ArrayStack<int>();
+
+            // Assert
+            Assert.True(arrayStack.IsEmpty);
+        }
+
+        [Fact]
+        public void ArrayStack_Push_Test()
+        {
+            // Arrange
+            var arrayStack = new ArrayStack<int>();
+
+            // Act
+            arrayStack.Push(1);
+
+            // Assert
+            Assert.False(arrayStack.IsEmpty);
+            Assert.Equal(1, arrayStack.Count);
+        }
+
+        [Fact]
+        public void ArrayStack_Peek_Test()
+        {
+            // Arrange
+            var arrayStack = new ArrayStack<int>();
+
+            // Act
+            arrayStack.Push(1);
+            var item = arrayStack.Peek();
+
+            // Assert
+            Assert.False(arrayStack.IsEmpty);
+            Assert.Equal(1, item);
+        }
+
+        [Fact]
+        public void ArrayStack_Pop_Test()
+        {
+            // Arrange
+            var arrayStack = new ArrayStack<int>();
+
+            // Act
+            arrayStack.Push(1);
+            arrayStack.Push(2);
+            arrayStack.Push(3);
+            var item = arrayStack.Pop();
+
+            // Assert
+            Assert.Equal(2, arrayStack.Count);
+            Assert.Equal(3, item);
+        }
+    }
+}
